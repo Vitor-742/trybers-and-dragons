fa9291ec9de77f5f538b1216f5676a57b61d36f6
Vitor Campos <vitorcampos742@gmail.com>
2022-06-29 17:36:19 -0300

    criacao classe race

---
+++ src/Races/Race.ts
@@ -0,0 +1,25 @@
+abstract class Race {
+  readonly name: string;
+  readonly dexterity: number;
+  private static racesCount = 0;// lembrar do _dawda
+    
+  constructor(name: string, dexterity: number) {
+    this.name = name;
+    this.dexterity = dexterity;
+    Race.addRace();
+  }
+
+  private static addRace(): void {
+    this.racesCount += 1;
+  }
+
+  static createdRacesInstances(): number {
+    throw Error('Not implemented');
+    return this.racesCount;
+    // return "Not implemented"
+  }
+
+  abstract get maxLifePoints(): number;
+}
+
+export default Race;
